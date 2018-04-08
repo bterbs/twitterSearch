405ee0857c5db2a546aa2e15234acd1d4e9b48e9
B Terbush <brittanyterbush@gmail.com>
2018-04-08 15:06:29 -0700

    WIP:Declare function to get tweets

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,15 +1,29 @@
+const Twitter = require('twitter');
+
+function getTweets(searchParams) {
+  var client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  });
+
+  var params = {screen_name: 'sfbaebleton'};
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+      console.log(tweets);
+    }
+  });
+
+}
+
+
 $(document).ready(function() {
   console.log('The DOM is ready, B.')
 
   $('search-btn').on('click', () => {
     //let formData = $('#search-box').val();
     //getTweets(formData);
-    var client = new Twitter({
-      consumer_key: process.env.TWITTER_CONSUMER_KEY,
-      consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-      access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-      access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-    });
     console.log('you clicked the search button!');
   });
 
